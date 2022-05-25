4e362e0e8c4471b66e0056873d33a4b1931dcce3
natsumerin <64072399+natsumerinchan@users.noreply.github.com>
2022-05-25 22:37:09 +0800

    Change file encoding to UTF-8
    Change file encoding to UTF-8 and change Language to English,because Chinese will be garbled character on UTF-8 bat.
    将文本编码改为UTF-8，语言改为英文，因为中文在UTF-8编码的批处理中会乱码。

--- Grasscutter_Resources_Updater.bat
+++ Grasscutter_Resources_Updater.bat
@@ -2,23 +2,24 @@
 if exist ".\Grasscutter_Resources" (
   if exist ".\Grasscutter\Resources" (
     move ./Grasscutter/Resources ./Grasscutter_Resources/Resources
-    cd ./Grasscutter_Resources && git pull origin main && move Resources ../Grasscutter/Resources && echo 更新完毕
+    cd ./Grasscutter_Resources && git pull origin main && move Resources ../Grasscutter/Resources && echo Update completed
   ) else (
     if exist ".\Grasscutter_Resources\Resources" (
-      cd ./Grasscutter_Resources && git pull origin main && move Resources ../Grasscutter/Resources && echo 更新完毕
+      cd ./Grasscutter_Resources && git pull origin main && move Resources ../Grasscutter/Resources && echo Update completed
     ) else (
       rd /S /Q ".\Grasscutter_Resources"
       git clone https://github.com/Koko-boya/Grasscutter_Resources.git
-      cd ./Grasscutter_Resources && git pull origin main && move Resources ../Grasscutter/Resources && echo 更新完毕
+      cd ./Grasscutter_Resources && git pull origin main && move Resources ../Grasscutter/Resources && echo Update completed
     )
   )
 ) else (
   if exist ".\Grasscutter\Resources" (
-    echo 删除旧Resources
+    echo Delete the original Resources folder
     rd /S /Q .\Grasscutter\Resources
+    echo Finished.
   )
   git clone https://github.com/Koko-boya/Grasscutter_Resources.git
-  cd ./Grasscutter_Resources && git pull origin main && move Resources ../Grasscutter/Resources && echo 更新完毕
+  cd ./Grasscutter_Resources && git pull origin main && move Resources ../Grasscutter/Resources && echo Update completed
 )
 pause
 
