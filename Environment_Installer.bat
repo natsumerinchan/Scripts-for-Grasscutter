20758e07ae8db299c3901ded193b19266b981116
natsumerin <64072399+natsumerinchan@users.noreply.github.com>
2022-05-25 22:46:54 +0800

    Change file encoding to UTF-8
    Change file encoding to UTF-8 and change Language to English,because Chinese will be garbled character on UTF-8 bat.
    将文本编码改为UTF-8，语言改为英文，因为中文在UTF-8编码的批处理中会乱码。

--- Environment_Installer.bat
+++ Environment_Installer.bat
@@ -1,21 +1,21 @@
 @echo off
-echo 本脚本依赖Scoop，未安装的将自动进行安装并重新执行脚本
-echo 此脚本会提示获取管理员权限两次，第一次是将MongoDB设置为系统服务，第二次是启动MongoDB服务
+echo This script depends on Scoop, if it is not installed, it will install automatically and re-execute the script.
+echo This script will prompt for administrator privileges twice, the first time to set up MongoDB as a system service, and the second time to start the MongoDB service.
 
-set /p Choose1=是否继续安装(Y/N):
-set /p Choose2=安装完毕后是否立即重启系统(Y/N):
+set /p Choose1=Do you want to continue?(Y/N):
+set /p Choose2=Do you want to reboot the system immediately after installation?(Y/N):
 if %Choose1% == Y (
-echo 正在开始安装
+echo Installation is starting now......
 goto NEXT1
 ) else (
-echo 已取消安装
+echo Canceled installation.
 pause 
 exit
 )
 :NEXT1 && (
 set path=%USERPROFILE%\scoop\shims;C:\ProgramData\scoop\shims;%path% ) && (
 if exist "%USERPROFILE%\scoop\shims\scoop" (
-echo scoop已安装
+echo Scoop is installed.
 goto NEXT2
 ) else (
 powershell "iwr -useb get.scoop.sh | iex"
@@ -27,17 +27,17 @@ scoop bucket add main
 scoop bucket add extras
 scoop bucket add java
 scoop install aria2 sudo
-echo Scoop已安装完毕，将重新运行脚本
+echo Scoop has been installed and will re-run this script.
 pause && %0
 )
 )
 :NEXT2 && (
-echo 正在安装环境包...... ) && (
+echo Installing environment package...... ) && (
 scoop update ) && (
 scoop install oraclejdk@17.0.3.1 ) && (
 scoop install mongodb mongodb-compass mitmproxy ) && (
-echo 正在设置mongodb...... ) && (
-echo 正在设置环境变量 ) && (
+echo Setting up mongodb...... ) && (
+echo Setting up environment variables...... ) && (
 setx MONGOPATH "%USERPROFILE%\scoop\apps\mongodb\current" ) && (
 set MONGOPATH=%USERPROFILE%\scoop\apps\mongodb\current ) && (
 copy nul "%MONGOPATH%\log\mongodb.log" ) & (
@@ -49,13 +49,13 @@ echo logpath=%MONGOPATH%\log\mongodb.log>>"%MONGOPATH%\mongo.config" ) ) && (
 sudo "%MONGOPATH%\bin\mongod.exe" --bind_ip 0.0.0.0 --logpath "%MONGOPATH%\log\mongodb.log" --logappend --dbpath "%MONGOPATH%\data\db" --serviceName "MongoDB" --serviceDisplayName "MongoDB" --install ) && (
 sudo net start MongoDB ) && (
 if %Choose2% == Y (
-echo 安装成功
-echo 你已经选择自动重启，若不想重启请在右上角直接关闭窗口（勿使用Ctrl + C或Ctrl + Z）
+echo Installed successfully
+echo You have chosen to reboot automatically, if you do not want to reboot please close the window directly in the upper right corner (do not use Ctrl + C or Ctrl + Z).
 pause
 shutdown -r -t 0
 ) else (
-echo 安装成功
-echo 请自行重启系统
+echo Installed successfully
+echo Please reboot the system yourself.
 pause
 )
 )
