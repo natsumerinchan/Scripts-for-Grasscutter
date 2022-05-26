7fc0789139a7c5918446e7c057293e112691e8b3
natsumerin <64072399+natsumerinchan@users.noreply.github.com>
2022-05-26 18:57:37 +0800

    update

--- Grasscutter_Resources_Updater.bat
+++ Grasscutter_Resources_Updater.bat
@@ -2,24 +2,24 @@
 if exist ".\Grasscutter_Resources" (
   if exist ".\Grasscutter\Resources" (
     move ./Grasscutter/Resources ./Grasscutter_Resources/Resources
-    cd ./Grasscutter_Resources && git pull origin main && move Resources ../Grasscutter/Resources && echo Update completed
+    cd ./Grasscutter_Resources && git pull origin main && move Resources ../Grasscutter/Resources && echo [INFO] Update completed
   ) else (
     if exist ".\Grasscutter_Resources\Resources" (
-      cd ./Grasscutter_Resources && git pull origin main && move Resources ../Grasscutter/Resources && echo Update completed
+      cd ./Grasscutter_Resources && git pull origin main && move Resources ../Grasscutter/Resources && echo [INFO] Update completed
     ) else (
       rd /S /Q ".\Grasscutter_Resources"
       git clone https://github.com/Koko-boya/Grasscutter_Resources.git
-      cd ./Grasscutter_Resources && git pull origin main && move Resources ../Grasscutter/Resources && echo Update completed
+      cd ./Grasscutter_Resources && git pull origin main && move Resources ../Grasscutter/Resources && echo [INFO] Update completed
     )
   )
 ) else (
   if exist ".\Grasscutter\Resources" (
-    echo Delete the original Resources folder
+    echo [INFO] Deleting the original Resources folder......
     rd /S /Q .\Grasscutter\Resources
-    echo Finished.
+    echo [INFO] Finished.
   )
   git clone https://github.com/Koko-boya/Grasscutter_Resources.git
-  cd ./Grasscutter_Resources && git pull origin main && move Resources ../Grasscutter/Resources && echo Update completed
+  cd ./Grasscutter_Resources && git pull origin main && move Resources ../Grasscutter/Resources && echo [INFO] Update completed
 )
 pause
 
--- Grasscutter_Updater.bat
+++ Grasscutter_Updater.bat
@@ -2,23 +2,23 @@
 :ifexist
 if exist ".\Grasscutter" (
   if exist ".\Grasscutter\grasscutter.jar" (
-    echo Deleting the original grasscutter.jar ......
+    echo [INFO] Deleting the original grasscutter.jar......
     del /q ".\Grasscutter\grasscutter.jar"
-    echo Deleted.
+    echo [INFO] Deleted.
     goto gitpull
   ) else (
     goto gitpull
   )
 ) else (
-  echo Cloning the development branch ......
+  echo [INFO] Cloning the development branch......
   git clone -b development https://github.com/Grasscutters/Grasscutter.git
   goto ifexist
 )
 :gitpull
-echo Pulling updates from the development branch && (
+echo [INFO] Pulling updates from the development branch && (
 cd ./Grasscutter
 git pull origin development 
-echo Finished.
+echo [INFO] Finished.
 gradlew.bat 
 gradlew jar
 move grasscutter-*-dev.jar ./grasscutter.jar 
