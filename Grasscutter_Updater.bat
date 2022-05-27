f932db857c5b3382bc9fb16b7109fecbb6733c6a
natsumerin <64072399+natsumerinchan@users.noreply.github.com>
2022-05-27 22:55:00 +0800

    Support change branch

--- Grasscutter_Updater.bat
+++ Grasscutter_Updater.bat
@@ -1,4 +1,12 @@
 @echo off
+@rem If you wnat to change to dev-world-scripts branch,please "set BRANCH=dev-world-scripts" 
+@rem Branches List:
+@rem stable
+@rem development
+@rem dev-world-scripts
+
+set BRANCH=development
+
 :ifexist
 if exist ".\Grasscutter" (
   if exist ".\Grasscutter\grasscutter.jar" (
@@ -10,18 +18,29 @@ if exist ".\Grasscutter" (
     goto gitpull
   )
 ) else (
-  echo [INFO] Cloning the development branch......
-  git clone -b development https://github.com/Grasscutters/Grasscutter.git
+  echo [INFO] Cloning the %BRANCH% branch......
+  git clone -b %BRANCH% https://github.com/Grasscutters/Grasscutter.git
   goto ifexist
 )
 :gitpull
-echo [INFO] Pulling updates from the development branch && (
 cd ./Grasscutter
-git pull origin development 
+for /F "delims=" %%n in ('git branch --show-current') do set "CURRENT=%%n"
+echo [INFO] Current Branch: %CURRENT%
+if not %CURRENT% == %BRANCH% (
+  echo [INFO] Changing branch to %BRANCH%
+  if exist ".\config.json" ( del /q ".\config.json" )
+  if exist ".\data" ( rd /S /Q ".\data" )
+  git checkout %BRANCH% >nul || git checkout -b %BRANCH% origin/%BRANCH% >nul
+  git reset --hard HEAD
+)
+echo [INFO] Pulling updates from the %BRANCH% branch && (
+git pull origin %BRANCH%:%BRANCH%
 echo [INFO] Finished.
+echo [INFO] Buliding jar......
 gradlew.bat 
 gradlew jar
 move grasscutter-*-dev.jar ./grasscutter.jar 
+echo [INFO] Finished.
 pause 
 )
 
