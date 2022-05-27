4778d104e21060292b9f47ec19a3486b0bce8603
natsumerin <64072399+natsumerinchan@users.noreply.github.com>
2022-05-27 14:53:34 +0800

    Update Game_Start.bat

--- Launch Scripts/Game_Start.bat
+++ Launch Scripts/Game_Start.bat
@@ -3,7 +3,7 @@
 echo [INFO] Setting proxy......
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f >nul
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /d "127.0.0.1:23121" /f >nul
-echo [INFO] Proxy settings is completed!
+echo [INFO] Proxy settings are completed!
 
 start .\Grasscutter_Start.bat 
 
@@ -11,7 +11,7 @@ start .\Grasscutter_Start.bat
 tasklist|find /i "mitmdump.exe" >nul
 if %errorlevel%==0 (
 choice /t 5 /d y /n >nul
-start .\Genshin.lnk 
+start .\Genshin.lnk || ( echo [ERROR] Game is not started! && goto EXIT0 )
 echo [INFO] Game is started!
 goto NEXT1
 ) else (
@@ -19,7 +19,7 @@ goto LOOP1
 )
 
 :NEXT1
-choice /t 15 /d y /n >nul
+choice /t 5 /d y /n >nul
 tasklist|find /i "GenshinImpact.exe" >nul
 if %errorlevel%==0 (
    set GAMEEXE=GenshinImpact.exe >nul
@@ -35,16 +35,18 @@ tasklist|find /i "%GAMEEXE%" >nul
 if %errorlevel%==0 (
    goto LOOP2
 ) else (
-   echo [INFO] Game is exited!
-   echo [INFO] Clearing proxy settings......
-   reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f >nul 
-   reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /d "" /f >nul
-   echo [INFO] Proxy settings is cleared!
-   taskkill /F /im mitmdump.exe >nul
-   echo [INFO] Proxy server is killed!
-   taskkill /F /im "java.exe" >nul
-   echo [INFO] Grasscutter server is stoped!
-   echo Please press any key to stop the terminal windows......
-   pause >nul
-   taskkill /F /im "WindowsTerminal.exe" >nul & taskkill /F /im "cmd.exe" >nul
+   echo [INFO] Game is stoped!
+   goto EXIT0
 )
+
+:EXIT0
+echo [INFO] Clearing proxy settings......
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f >nul 
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /d "" /f >nul
+echo [INFO] Proxy settings are cleared!
+taskkill /F /im mitmdump.exe >nul
+echo [INFO] Proxy server is killed!
+taskkill /F /im "java.exe" >nul
+echo [INFO] Grasscutter server is stoped! 
+taskkill /F /im "WindowsTerminal.exe" >nul & taskkill /F /im "cmd.exe" >nul
+
